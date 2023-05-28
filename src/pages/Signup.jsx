f0fb09f58f46ad19428da3537aee12a25dd40d15
Ji-Tae <117277785+Ji-Tae@users.noreply.github.com>
2023-05-28 17:43:50 +0900

    [Docs]userinput 폴더와 파일 삭제

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
 import { majorGet, signupPost } from '../api/users';
 import { useNavigate } from 'react-router-dom';
